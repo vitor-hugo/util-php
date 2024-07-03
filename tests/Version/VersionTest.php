dd807e9b5e89dcef326a408aad29be49bd5c72d9
vitor-hugo <vitor83dev@gmail.com>
2024-07-03 14:12:35 -0300

    init: Version tests

---
+++ tests/Version/VersionTest.php
@@ -0,0 +1,122 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Version;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use Torugo\Util\Enums\VersionComparison;
+use Torugo\Util\Version\Version;
+
+#[Group("Version")]
+#[TestDox("Version")]
+class VersionTest extends TestCase
+{
+    #[TestDox("Should extract version parts correctly")]
+    public function testShouldBeValid()
+    {
+        $version = new Version("1.1.0-alpha+134");
+        $this->assertEquals(1, $version->major);
+        $this->assertEquals(1, $version->minor);
+        $this->assertEquals(0, $version->patch);
+        $this->assertEquals('alpha', $version->preRelease);
+        $this->assertEquals(134, $version->build);
+    }
+
+
+    #[TestDox("Should be smaller")]
+    public function testShouldBeSmaller()
+    {
+        $versions = [
+            ["0.0.1", "1.0.1"],
+            ["1.0.0", "1.0.1"],
+            ["1.0.9", "1.1.0"],
+            ["1.0.0", "1.1.0"],
+            ["1.0.0", "2.0.0"],
+            ["1.0.0-alpha", "1.0.0-alpha.1"],
+            ["1.0.0-alpha.1", "1.0.0-alpha.2"],
+            ["1.0.0-alpha", "1.0.0-beta"],
+            ["1.0.0-alpha.1", "1.0.0-beta"],
+            ["1.0.0-beta", "1.0.0-beta.1"],
+            ["1.0.0-beta.1", "1.0.0-beta.2"],
+            ["1.0.0-beta", "1.0.0-rc"],
+            ["1.0.0-beta.1", "1.0.0-rc"],
+            ["1.0.0-rc", "1.0.0-rc.1"],
+            ["1.0.0-rc", "1.0.0"],
+            ["1.0.0-rc.1", "1.0.0"],
+            ["1.0.0+11", "1.0.0+12"],
+            ["1.0.0-beta.2+167", "1.0.0-beta.2+234"],
+        ];
+
+        foreach ($versions as $v) {
+            $version = new Version($v[0]);
+            $this->assertEquals(VersionComparison::Smaller, $version->compareTo($v[1]));
+        }
+    }
+
+
+    #[TestDox("Should be bigger")]
+    public function testShouldBeBigger()
+    {
+        $versions = [
+            ["1.0.1", "1.0.0"],
+            ["1.1.0", "1.0.0"],
+            ["2.0.0", "1.0.0"],
+            ["1.0.0-alpha.1", "1.0.0-alpha"],
+            ["1.0.0-alpha.2", "1.0.0-alpha.1"],
+            ["1.0.0-beta", "1.0.0-alpha"],
+            ["1.0.0-beta", "1.0.0-alpha.1"],
+            ["1.0.0-beta.1", "1.0.0-beta"],
+            ["1.0.0-beta.2", "1.0.0-beta.1"],
+            ["1.0.0-rc", "1.0.0-beta"],
+            ["1.0.0-rc", "1.0.0-beta.1"],
+            ["1.0.0-rc.1", "1.0.0-rc"],
+            ["1.0.0", "1.0.0-rc"],
+            ["1.0.0", "1.0.0-rc.1"],
+            ["3.2.6+145", "3.2.6+123"],
+            ["1.0.0+600", "1.0.0-alpha.1+650"],
+            ["1.0.0-alpha.2+988", "1.0.0-alpha.2+987"],
+        ];
+
+        foreach ($versions as $v) {
+            $version = new Version($v[0]);
+            $this->assertEquals(VersionComparison::Bigger, $version->compareTo($v[1]));
+        }
+    }
+
+
+    #[TestDox("Should be equal")]
+    public function testShouldBeEqual()
+    {
+        $versions = [
+            ["1.0.0", "1.0.0"],
+            ["1.0.1", "1.0.1"],
+            ["1.1.0", "1.1.0"],
+            ["1.1.1", "1.1.1"],
+            ["1.1.1+13", "1.1.1+13"],
+            ["1.0.0-alpha", "1.0.0-alpha"],
+            ["1.0.0-alpha.1", "1.0.0-alpha.1"],
+            ["1.0.0-beta", "1.0.0-beta"],
+            ["1.0.0-beta.1", "1.0.0-beta.1"],
+            ["1.0.0-beta.2", "1.0.0-beta.2"],
+            ["1.0.0-rc", "1.0.0-rc"],
+            ["1.0.0-rc.1", "1.0.0-rc.1"],
+            ["1.0.0-alpha.2+988", "1.0.0-alpha.2+988"],
+        ];
+
+        foreach ($versions as $v) {
+            $version = new Version($v[0]);
+            $this->assertEquals(VersionComparison::Equal, $version->compareTo($v[1]));
+        }
+    }
+
+
+    #[TestDox("Should throw InvalidArgumentException on invalid version number")]
+    public function testThrowOnInvalidVersion()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Invalid version number '1.0'.");
+        new Version("1.0");
+    }
+}
