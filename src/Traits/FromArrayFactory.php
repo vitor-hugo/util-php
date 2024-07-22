4193e5b48c885abb761831a4127b8a8c2df99703
vitor-hugo <vitor83dev@gmail.com>
2024-07-22 12:11:45 -0300

    init: FromArrayFactory trait

---
+++ src/Traits/FromArrayFactory.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Util\Traits;
+
+use ReflectionObject;
+
+trait FromArrayFactory
+{
+    use EmptyValues;
+
+    /**
+     * Instantiates a class from a key=>value array.
+     * The keys must be equal to the properties names.
+     * @return object
+     */
+    public static function fromArray(array $data): self
+    {
+        $obj = new self;
+        $props = self::getProperties($obj);
+
+        foreach ($props as $prop) {
+            $propName = $prop->getName();
+            $propType = $prop->getType();
+
+            $type = "unknown";
+            $isNullable = false;
+
+            if ($propType != null && method_exists($propType, "getName")) {
+                $type = $propType->getName();
+                $isNullable = $propType->allowsNull();
+            }
+
+            if (array_key_exists($propName, $data)) {
+                $obj->{$propName} = $data[$propName];
+                continue;
+            }
+
+            // If the property is not present on $data and doesn't has a default value
+            if ($prop->hasDefaultValue() == false) {
+                if ($isNullable) {
+                    $obj->{$propName} = null;
+                } else {
+                    $obj->{$propName} = self::getEmptyValueForType($type);
+                }
+            }
+
+        }
+
+        return $obj;
+    }
+
+    /**
+     * Get class properties as an array of ReflectionProperties
+     * @param object $obj
+     * @return array ReflectionProperties[]
+     */
+    private static function getProperties(object $obj): array
+    {
+        $reflection = new ReflectionObject($obj);
+        $properties = $reflection->getProperties();
+        return $properties;
+    }
+}
