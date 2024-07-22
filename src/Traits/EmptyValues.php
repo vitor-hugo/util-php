b7715a6beea3cda8e153e34df225dfd2bfee8dc8
vitor-hugo <vitor83dev@gmail.com>
2024-07-22 12:16:08 -0300

    init: EmptyValues trait

---
+++ src/Traits/EmptyValues.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\Util\Traits;
+
+trait EmptyValues
+{
+    /**
+     * Returns an empty value for a specific type
+     * @param string $type
+     * @return mixed Empty value
+     */
+    private static function getEmptyValueForType(string $type): mixed
+    {
+        $type = strtolower($type);
+
+        $empty = match ($type) {
+            "array" => [],
+            "boolean", "bool" => false,
+            "double", "float" => 0.0,
+            "integer", "int" => 0,
+            "object" => new class {},
+            "string" => "",
+            default => null
+        };
+
+        return $empty;
+    }
+}
