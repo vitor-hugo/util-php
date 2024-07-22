510af58ba0820170d86b62d5535ec12699391746
vitor-hugo <vitor83dev@gmail.com>
2024-07-22 10:42:38 -0300

    refact: changing write function to accept all characters defined in DateTimeInterface::format

--- src/DateWriter/DateWriter.php
+++ src/DateWriter/DateWriter.php
@@ -9,6 +9,11 @@ use Intl;
 
 class DateWriter
 {
+    /**
+     * Transforms DateTime objects to written date/time.
+     * @param \DateTime $dateTime PHP DateTime instance
+     * @param string $language Language used to write the names of months and days of the week
+     */
     public function __construct(private DateTime $dateTime, private string $language = "en")
     {
         $this->loadLanguage($language);
@@ -28,29 +33,67 @@ class DateWriter
     }
 
 
+    /**
+     * Summary of write
+     * @param string $format
+     * @return string
+     */
     public function write(string $format): string
     {
         $lang = $this->language;
         $dt = $this->dateTime;
 
+        $ordinals = [
+            "st" => 0,
+            "nd" => 1,
+            "rd" => 2,
+            "th" => 3,
+        ];
+
+        $ordinal = $ordinals[$dt->format("S")];
+
         $options = [
             "d" => $dt->format("d"),
-            "D" => (int) $dt->format("d"),
+            "j" => $dt->format("j"),
+            "D" => Intl::SHORT_WEEK_DAY[$lang][$dt->format("w")],
+            "l" => Intl::LONG_WEEK_DAY[$lang][$dt->format("w")],
+            "N" => $dt->format("N"),
+            "S" => Intl::ORDINAL_SUFFIX[$lang][$ordinal],
+            "w" => $dt->format("w"),
+            "z" => $dt->format("z"),
+            "W" => $dt->format("W"),
             "m" => $dt->format("m"),
-            "M" => $dt->format("n"),
-            "o" => Intl::LONG_MONTH[$lang][$dt->format("n") - 1],
-            "n" => Intl::SHORT_MONTH[$lang][$dt->format("n") - 1],
-            "w" => Intl::LONG_WEEK_DAY[$lang][$dt->format("w")],
-            "e" => Intl::SHORT_WEEK_DAY[$lang][$dt->format("w")],
-            "y" => $dt->format("y"),
+            "n" => $dt->format("n"),
+            "F" => Intl::LONG_MONTH[$lang][$dt->format("n") - 1],
+            "M" => Intl::SHORT_MONTH[$lang][$dt->format("n") - 1],
+            "t" => $dt->format("t"),
+            "L" => $dt->format("L"),
+            "o" => $dt->format("o"),
+            "X" => $dt->format("X"),
+            "x" => $dt->format("x"),
             "Y" => $dt->format("Y"),
-            "h" => $dt->format("G"),
+            "y" => $dt->format("y"),
+            "a" => $dt->format("a"),
+            "A" => $dt->format("A"),
+            "B" => $dt->format("B"),
+            "g" => $dt->format("g"),
+            "G" => $dt->format("G"),
+            "h" => $dt->format("h"),
             "H" => $dt->format("H"),
             "i" => $dt->format("i"),
             "s" => $dt->format("s"),
             "u" => $dt->format("u"),
-            "a" => $dt->format("a"),
-            "A" => $dt->format("A"),
+            "v" => $dt->format("v"),
+            "e" => $dt->format("e"),
+            "I" => $dt->format("I"),
+            "O" => $dt->format("O"),
+            "P" => $dt->format("P"),
+            "p" => $dt->format("p"),
+            "T" => $dt->format("T"),
+            "Z" => $dt->format("Z"),
+            "c" => $dt->format("c"),
+            "r" => $dt->format("r"),
+            "U" => $dt->format("U"),
         ];
 
         $formatArray = str_split($format, 1);
@@ -83,9 +126,9 @@ class DateWriter
 
     private function resolveUppercaseMarks(string $value): string
     {
-        $value = preg_replace_callback("/U{.*}/", function ($matches) {
+        $value = preg_replace_callback("/\*{.*}/", function ($matches) {
             $v = $matches[0];
-            $v = str_replace(["U{", "}"], "", $v);
+            $v = str_replace(["*{", "}"], "", $v);
             $v = mb_strtoupper($v);
             return $v;
         }, $value);
@@ -96,9 +139,9 @@ class DateWriter
 
     private function resolveLowercaseMarks(string $value): string
     {
-        $value = preg_replace_callback("/L{.*}/", function ($matches) {
+        $value = preg_replace_callback("/\%{.*}/", function ($matches) {
             $v = $matches[0];
-            $v = str_replace(["L{", "}"], "", $v);
+            $v = str_replace(["%{", "}"], "", $v);
             $v = mb_strtolower($v);
             return $v;
         }, $value);
